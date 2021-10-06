7ffd29779de0cf4e7bd698cdaf4e1365319ec7d0
Stas <stasscript@gmail.com>
2021-10-06 08:57:05 +0300

    archive notes

--- script.js
+++ script.js
@@ -1,41 +1,72 @@
+// variables and start point
+
 const list = document.querySelector('.list'),
   form = document.querySelector('.form'),
   createBtn = document.querySelector('.create'),
-  closeBtn = document.querySelector('.close-form');
-let elementsCounter = 0;
+  closeBtn = document.querySelector('.close-form'),
+  getArchiveBtn = document.querySelector('.get-archived'),
+  archiveBlock = document.querySelector('.archive'),
+  archivedList = document.querySelector('.archived-list');
+let elementsCounter = 0,
+  archivedElementsCounter = 0;
 
 let notes = [];
+let archivedNotes = [];
 
-function deleteItem(id) {
-  let newArr = [...notes.slice(0, id), ...notes.slice(id + 1)];
-  newArr.forEach((item, index) => {
-    if (index >= id) item.id--;
-  });
-  notes = newArr;
-  updateList();
-}
+updateList();
 
-function generateString() {
-  if (notes.length === 0) {
+//event listeners
+
+form.addEventListener('submit', addItem);
+
+createBtn.addEventListener('click', () => {
+  form.classList.toggle('hide');
+});
+
+closeBtn.addEventListener('click', () => {
+  form.classList.add('hide');
+});
+
+getArchiveBtn.addEventListener('click', () => {
+  archiveBlock.classList.toggle('hide');
+});
+
+//renderFunctions
+
+function generateString(arr) {
+  if (arr.length === 0) {
     return `<tr><td>You don't have any notes</td></tr>`;
   } else {
     let string = '';
-    notes.map(({ text, time, id, category, dates }) => {
-      string += `<tr><td>${time}</td><td contenteditable>${text}</td><td>${category}</td><td>${dates}</td>
-      <td>
-      <button class="archive btn btn-secondary" onClick="archiveItem(${id})">Archive</button>
-      <button class="delete btn btn-danger" onClick="deleteItem(${id})">X</button>
-      </td></tr>`;
+    arr.map(({ text, time, id, category, dates }) => {
+      let tools;
+      if (arr === notes) {
+        tools = `<button class="archive btn btn-secondary" onClick="archiveItem(${id})">Archive</button>
+      <button class="delete btn btn-danger" onClick="deleteItem(${id})">X</button>`;
+      } else {
+        tools = `<button class="archive btn btn-secondary" onClick="unArchiveItem(${id})">Unarchive</button>`;
+      }
+      string += `<tr>
+      <td>${time}</td><td >${text}</td><td>${category}</td><td>${dates}</td>
+      <td>${tools}</td>
+      </tr>`;
     });
     return string;
   }
 }
 
 function updateList() {
-  let listText = generateString();
-  list.innerHTML = listText;
+  let notesListText = generateString(notes);
+  list.innerHTML = notesListText;
+  let archievedListText = generateString(archivedNotes);
+  archivedList.innerHTML = archievedListText;
   elementsCounter = notes.length;
+  archivedElementsCounter = archivedNotes.length;
   countNotes();
+  console.log('Notes:');
+  console.log(notes);
+  console.log('ArchievedNotes');
+  console.log(archivedNotes);
 }
 
 function countNotes() {
@@ -46,30 +77,30 @@ function countNotes() {
     ideaAct = 0,
     ideaArch = 0;
   notes.forEach((item) => {
-    if (item.active) {
-      switch (item.category) {
-        case 'Task':
-          tasksAct++;
-          break;
-        case 'Random thought':
-          rndAct++;
-          break;
-        case 'Idea':
-          ideaAct++;
-      }
-    } else {
-      switch (item.category) {
-        case 'Task':
-          tasksArch++;
-          break;
-        case 'Random thought':
-          rndArch++;
-          break;
-        case 'Idea':
-          ideaArch++;
-      }
+    switch (item.category) {
+      case 'Task':
+        tasksAct++;
+        break;
+      case 'Random thought':
+        rndAct++;
+        break;
+      case 'Idea':
+        ideaAct++;
     }
   });
+  archivedNotes.forEach((item) => {
+    switch (item.category) {
+      case 'Task':
+        tasksArch++;
+        break;
+      case 'Random thought':
+        rndArch++;
+        break;
+      case 'Idea':
+        ideaArch++;
+    }
+  });
+
   const tasksActBlock = document.querySelector('.tasks-active'),
     tasksArchBlock = document.querySelector('.tasks-archived'),
     rndActBlock = document.querySelector('.thoughts-active'),
@@ -84,12 +115,48 @@ function countNotes() {
   ideaArchBlock.innerHTML = ideaArch;
 }
 
-updateList();
+//item handlers
 
-form.addEventListener('submit', (e) => {
+function archiveItem(id) {
+  let elem = JSON.parse(JSON.stringify(notes[id]));
+  let newNotesArr = [...notes.slice(0, id), ...notes.slice(id + 1)];
+  newNotesArr.forEach((item, index) => {
+    if (index >= id) item.id--;
+  });
+  notes = newNotesArr;
+  elem.id = archivedElementsCounter;
+  let newArchievedArr = [...archivedNotes, elem];
+  archivedNotes = newArchievedArr;
+  updateList();
+}
+
+function unArchiveItem(id) {
+  let elem = JSON.parse(JSON.stringify(archivedNotes[id]));
+  let newArchivedArr = [...archivedNotes.slice(0, id), ...archivedNotes.slice(id + 1)];
+  newArchivedArr.forEach((item, index) => {
+    if (index >= id) item.id--;
+  });
+  archivedNotes = newArchivedArr;
+  elem.id = elementsCounter;
+  let newNotesArr = [...notes, elem];
+  notes = newNotesArr;
+  updateList();
+}
+
+function deleteItem(id) {
+  let newArr = [...notes.slice(0, id), ...notes.slice(id + 1)];
+  newArr.forEach((item, index) => {
+    if (index >= id) item.id--;
+  });
+  notes = newArr;
+  updateList();
+}
+
+function addItem(e) {
   e.preventDefault();
-  let time = new Date().toLocaleTimeString();
   let data = e.target.elements;
+  if (data[0].value === '') return;
+  let time = new Date().toLocaleTimeString();
   let datePattern = /\d+\/\d+\/\d+/;
   let dates = datePattern.exec(data[0].value) || '-';
   notes.push({
@@ -98,16 +165,7 @@ form.addEventListener('submit', (e) => {
     category: data[1][data[1].options.selectedIndex].label,
     dates,
     id: elementsCounter,
-    active: true,
   });
   updateList();
   e.target.elements[0].value = '';
-});
-
-createBtn.addEventListener('click', () => {
-  form.classList.remove('hide');
-});
-
-closeBtn.addEventListener('click', () => {
-  form.classList.add('hide');
-});
+}
